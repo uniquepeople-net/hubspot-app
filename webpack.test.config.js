afa561b74be2bce22febf612b73ada31acf14cb0
Miroslav Viazanko <Miroslav.Viazanko@uniquepeople.net>
2023-01-19 14:16:52 +0100

    fix domain_url api-gw

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -85,7 +85,7 @@ module.exports = {
         }),
 		new webpack.DefinePlugin({
 			//DOMAIN_URL: JSON.stringify('https://ufp-dev-api-gw.uniquekube.uniquepeople.cloud'),
-			DOMAIN_URL: JSON.stringify('https://ufp-gw.uniquepeople.cloud'),
+			DOMAIN_URL: JSON.stringify('https://ufp-apigw.uniquepeople.cloud'),
 		}),
         new webpack.HotModuleReplacementPlugin(),
     ],
