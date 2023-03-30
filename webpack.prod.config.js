1d6e15f4473c50d61496972f784a6f9324f68c8f
Miroslav Viazanko <Miroslav.Viazanko@uniquepeople.net>
2023-03-30 13:08:02 +0200

    init prod

---
+++ webpack.prod.config.js
@@ -0,0 +1,92 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+//const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin');
+// const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const { VueLoaderPlugin } = require('vue-loader')
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: {
+        app: ['./src/main.js'],
+       	//mazer: ['./src/mazer.js'],
+    },
+    resolve: {
+        extensions: [ '.js', '.vue' ],
+        alias: {
+            '~': path.resolve(__dirname, 'src')
+          }
+    },
+    devtool: "eval",
+    output: {
+        filename: "[name].js",
+        path: path.resolve(__dirname, "./dist"),
+		publicPath: '/'
+    },
+    module: {
+        rules: [
+            { 
+                test: /\.js$/, 
+                use: 'babel-loader' 
+            },
+            {
+				test: /\.vue$/,
+				loader: 'vue-loader',
+				options: {
+				  loaders: {
+					// Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+					// the "scss" and "sass" values for the lang attribute to the right configs here.
+					// other preprocessors should work out of the box, no loader config like this necessary.
+					'scss': [
+					  'vue-style-loader',
+					  'css-loader',
+					  'sass-loader'
+					]
+				  }
+				  // other vue-loader options go here
+				}
+			  },
+            { 
+                test: /\.css$/, 
+                use: ['vue-style-loader', 'css-loader']
+            },
+			{
+				test: /\.scss$/,
+				use: [
+				  'vue-style-loader',
+				  'css-loader',
+				  'sass-loader'
+				]
+			},
+			{
+				test: /\.(jpg|png)$/,
+				use: {
+					loader: 'url-loader',
+				},
+			},
+        ]
+    },
+    devServer: {
+        open: true,
+        hot: true,
+        historyApiFallback: true,
+    },
+    // resolve: {
+    //     alias: {
+    //         'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js'  webpack 1용 입니다
+    //     }
+    // },
+    plugins: [
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+			inject: 'body',
+            template: './src/index.html',
+			/* chunks: ['app', 'mazer'],
+			chunksSortMode: 'manual', */
+        }),
+		new webpack.DefinePlugin({
+			//DOMAIN_URL: JSON.stringify('https://ufp-dev-api-gw.uniquekube.uniquepeople.cloud'),
+			DOMAIN_URL: JSON.stringify('https://app-apigw.ufp.sk'),
+		}),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+};
