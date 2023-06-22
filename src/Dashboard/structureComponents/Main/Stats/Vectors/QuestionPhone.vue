14dd3fc791f133d41ceb0e8b7944123edc607cd4
Miroslav Viazanko <Miroslav.Viazanko@uniquepeople.net>
2023-06-22 13:07:03 +0200

    fix color button

---
+++ src/Dashboard/structureComponents/Main/Stats/Vectors/QuestionPhone.vue
@@ -0,0 +1,33 @@
+<template>
+	<svg width="150" height="150" viewBox="0 0 150 150" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<g clip-path="url(#clip0_11_333)">
+		<path d="M93.75 131.25H50C46.6848 131.25 43.5054 129.933 41.1612 127.589C38.817 125.245 37.5 122.065 37.5 118.75V31.25C37.5 27.9348 38.817 24.7554 41.1612 22.4112C43.5054 20.067 46.6848 18.75 50 18.75H100C103.315 18.75 106.495 20.067 108.839 22.4112C111.183 24.7554 112.5 27.9348 112.5 31.25V68.75" stroke="black" stroke-width="11" stroke-linecap="round" stroke-linejoin="round"/>
+		<path d="M118.75 137.5V137.563" stroke="black" stroke-width="11.0833" stroke-linecap="round" stroke-linejoin="round"/>
+		<path d="M118.75 118.75C121.552 118.741 124.27 117.792 126.469 116.056C128.667 114.319 130.22 111.896 130.878 109.172C131.537 106.449 131.262 103.583 130.099 101.034C128.936 98.4851 126.951 96.4 124.462 95.1125C121.976 93.8388 119.132 93.4439 116.393 93.992C113.653 94.5401 111.18 95.9989 109.375 98.1312" stroke="black" stroke-width="11" stroke-linecap="round" stroke-linejoin="round"/>
+		<path d="M68.75 25H81.25" stroke="black" stroke-width="11.0833" stroke-linecap="round" stroke-linejoin="round"/>
+		<path d="M75 106.25V106.313" stroke="black" stroke-width="11" stroke-linecap="round" stroke-linejoin="round"/>
+		</g>
+		<defs>
+		<clipPath id="clip0_11_333">
+		<rect width="150" height="150" fill="white"/>
+		</clipPath>
+		</defs>
+	</svg>
+</template>
+ 
+ 
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		methods: {
+ 
+		},
+	}
+</script>
+ 
+ 
+<style lang='scss' scoped>
+</style>
